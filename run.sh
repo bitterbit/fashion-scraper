7383275ca40a935cc84ec8d9fb69182d3477d477
Amit Trivizki <amit.trivizki@gmail.com>
2020-02-21 12:34:27 +0200

    run-script: make run.sh cross-platfrom

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-docker run --rm -v $(PWD)/src:/src -it fashion-scraper /bin/bash
+case "$(uname -s)" in
+	   Darwin) # Mac OS :(
+		   pwdcmd="PWD";;
+	   Linux) # Linux :)
+		   pwdcmd="pwd";;
+	   *)
+		   pwdcmd="pwd";;
+esac
+
+curdir=$(eval $pwdcmd)
+docker run --rm -v $curdir/src:/src -it fashion-scraper /bin/bash

8afc656832330e0bfc050a09195176c1d23c7daf
Amit Trivizki <amit.trivizki@gmail.com>
2020-02-22 19:35:21 +0200

    human-filter: Add human filer

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM jjanzic/docker-python3-opencv:opencv-4.0.1
 
 RUN pip install scrapy
 RUN pip install furl pillow
+RUN pip install imutils
--- src/fashion_crawler/pipelines.py
+++ src/fashion_crawler/pipelines.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
 import scrapy
+from scrapy.exceptions import DropItem
 import hashlib
 import logging
+from fashion_crawler.settings import IMAGES_STORE
 from scrapy.pipelines.images import ImagesPipeline
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import imutils
+import cv2
 
 class HumanImageFilterPipline(ImagesPipeline):
     def item_completed(self, results, item, info):
@@ -16,5 +22,17 @@ class HumanImageFilterPipline(ImagesPipeline):
         return item
 
     def is_human(self, path):
-        print (path)
-        return True 
+        hog = cv2.HOGDescriptor()
+        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        path = os.path.join(IMAGES_STORE, path)
+        image = cv2.imread(path)
+        if image is None:
+            return False
+        
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        for w in weights:
+            if w > 1:
+                logging.debug("%s is person" % path )
+                return True
+        logging.debug("%s is NOT aperson" % path )
+        return False 

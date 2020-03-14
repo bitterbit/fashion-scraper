980f64fab5338e861991f0ade4339ae923cf9f7e
Gal Tashma <galtashma@gmail.com>
2020-03-15 01:52:33 +0200

    Updated predict to open images one by one

--- src/detector/predict.py
+++ src/detector/predict.py
@@ -3,7 +3,9 @@ import resizer
 import sys
 import cv2
 import os.path
+import os
 import numpy as np
+import random
 
 import IPython.display as display
 from PIL import Image
@@ -22,11 +24,19 @@ def load_image(path):
 def predict(model, img):
     probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
     predictions = probability_model.predict(img)[0]
-    # print("predictions!", predictions)
+    print("predictions!", predictions)
+
+    if max(predictions) < 0.4:
+        return "unknown"
+
+    diff = predictions[0] - predictions[1]
+    if diff > -0.1 and diff < 0.1:
+        return "unknown"
+    
     if predictions[0] > predictions[1]:
-        return "negative"
-    else:
         return "positive"
+    else:
+        return "negative"
     
                             
 def main():
@@ -37,9 +47,12 @@ def main():
     model = tf.keras.models.load_model(MODEL_PATH)
     img_dir = sys.argv[1]
 
-    for img_name in os.listdir(img_dir):
-        if img_name.endswith(".jpg"):
+    paths = os.listdir(img_dir)
+    print("paths", paths)
+    random.shuffle(paths)
 
+    for img_name in paths:
+        if img_name.endswith(".jpg"):
             path = os.path.join(img_dir, img_name)
             img = load_image(path)
             result = predict(model, img)
